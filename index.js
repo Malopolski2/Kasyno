57f32783d4038b67e64e844d2b68610e1e6f9502
Malopolski2 <150730432+Malopolski2@users.noreply.github.com>
2026-01-12 19:14:25 +0100

    Banner select + pull buttons

---
+++ index.js
@@ -0,0 +1,10 @@
+﻿function switchBanner(banner_id) {
+    let allBanners = document.querySelectorAll(".banner");
+    console.log(allBanners);
+    allBanners.forEach(el => {
+        el.style.display = "none";
+    });
+    let currentBanner = document.querySelector(`#${banner_id}`);
+    console.log(currentBanner);
+    currentBanner.style.display = "block";
+}
