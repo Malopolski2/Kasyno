10ecd3901724b9a3e1f1e0c2213d382c18caeea4
Malopolski2 <150730432+Malopolski2@users.noreply.github.com>
2026-01-16 17:18:32 +0100

    IT WORKS

--- index.js
+++ index.js
@@ -36,20 +36,20 @@ function Trainee(name, star, source) {
     this.source = source;
 }
 
-const fatty = new Trainee('Oguri Cap', '3 Star', 'images\t-fatty.jpg');
-const agemasen = new Trainee('Special Week', '3 Star', 'images\t-agemasen.jpg');
-const ballin = new Trainee('Tokai Teio', '3 Star', 'images\t-ballin.jpg');
-const rice = new Trainee('Rice Shower', '3 Star', 'images\t-rice.jpg');
-const falcon = new Trainee('Smart Falcon', '3 Star', 'images\t-falcon.jpg');
-const digital = new Trainee('Agnes Digital', '3 Star', 'images\t-digital.jpg');
+const fatty = new Trainee('Oguri Cap', '3 Star', 'images\\t-fatty.jpg');
+const agemasen = new Trainee('Special Week', '3 Star', 'images\\t-agemasen.jpg');
+const ballin = new Trainee('Tokai Teio', '3 Star', 'images\\t-ballin.jpg');
+const rice = new Trainee('Rice Shower', '3 Star', 'images\\t-rice.jpg');
+const falcon = new Trainee('Smart Falcon', '3 Star', 'images\\t-falcon.jpg');
+const digital = new Trainee('Agnes Digital', '3 Star', 'images\\t-digital.jpg');
 
-const mambo = new Trainee('Matikanetannhauser', '2 Star', 'images\t-mambo.jpg');
-const gold = new Trainee('Gold Ship', '2 Star', 'images\t-gold.jpg');
-const daiwa = new Trainee('Daiwa Scarlet', '2 Star', 'images\t-daiwa.jpg');
+const mambo = new Trainee('Matikanetannhauser', '2 Star', 'images\\t-mambo.jpg');
+const gold = new Trainee('Gold Ship', '2 Star', 'images\\t-gold.jpg');
+const daiwa = new Trainee('Daiwa Scarlet', '2 Star', 'images\\t-daiwa.jpg');
 
-const haru = new Trainee('Haru Urara', '1 Star', 'images\t-haru.jpg');
-const tachyon = new Trainee('Agnes Tachyon', '1 Star', 'images\t-tachyon.jpg');
-const nature = new Trainee('Nice Nature', '1 Star', 'images\t-nature.jpg');
+const haru = new Trainee('Haru Urara', '1 Star', 'images\\t-haru.jpg');
+const tachyon = new Trainee('Agnes Tachyon', '1 Star', 'images\\t-tachyon.jpg');
+const nature = new Trainee('Nice Nature', '1 Star', 'images\\t-nature.jpg');
 
 // Pulls
 
@@ -110,6 +110,24 @@ function TraineePull(banner_uma) {
                 return nature;
                 break;
         }
+    }
+}
+
+function TraineeBannerPull(banner_uma){
+    if(carrats >= 150) {
+        carrats += -150
+        let carratAmount = document.getElementById("carats_amount");
+        carratAmount.innerHTML = `${carrats} Carrats`;
+        let resultsDiv = document.getElementById("pull_results");
+        let resultsUma = TraineePull(banner_uma);
+        let resultsImage = new Image(150);
+        resultsImage.src = resultsUma.source;
+        resultsDiv.appendChild(resultsImage);
+    }
+}
+function TraineeBannerTenPull(banner_uma) {
+    for(let i = 0; i<10; i++) {
+        TraineeBannerPull(banner_uma);
     }
 }
 
